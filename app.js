883355084485fe17ab28fc7f6c2de2b3ccb383c3
omar abo el magd <omaraboelmagd4@gmail.com>
2026-09-13 21:47:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const getWeather = require("./data1/forecast");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("", (req, res) => {
     res.render("index");
